5013c62ce4205110f106a9d772bfeaaaf2df100d
Vishnu Sharma <90127810+vishnusharma7@users.noreply.github.com>
2023-06-23 14:54:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,49 +24,6 @@ function handleFileSelect(event) {
     fileInput.click(); // Simulate a click on the file input element
   });
 
-// Dark mode feature   dark mode, light mode , system default
-
-  const toggleButton = document.querySelector('.dark-mode-toggle');
-  const activeIcons = toggleButton.querySelector('.active');
-  const inactiveIcons = toggleButton.querySelector('.not-active');
-  const body = document.body;
-
-  toggleButton.addEventListener('click', function () {
-      if (inactiveIcons.style.display === 'none') {
-          inactiveIcons.style.display = 'flex';
-      } else {
-          inactiveIcons.style.display = 'none';
-      }
-  });
-
-  const notActiveIcons = document.querySelectorAll('.not-active > div');
-
-  notActiveIcons.forEach(function (icon) {
-      icon.addEventListener('click', function () {
-          const selectedIcon = icon.querySelector('svg');
-          const activeIcon = activeIcons.querySelector('.active-theme > svg');
-          activeIcon.replaceWith(selectedIcon.cloneNode(true));
-          inactiveIcons.style.display = 'none';
-          if (icon.classList.contains('light-theme')) {
-              body.classList.remove('dark');
-          } else if (icon.classList.contains('system-default')) {
-              if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                  body.classList.add('dark');
-              } else {
-                  body.classList.remove('dark');
-              }
-          } else {
-              body.classList.add('dark');
-          }
-      });
-  });
-  
-
-  document.addEventListener('click', function (event) {
-      if (!toggleButton.contains(event.target)) {
-          inactiveIcons.style.display = 'none';
-      }
-  });
 
 
 
