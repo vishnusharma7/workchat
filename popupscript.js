7869c77148868a84a4135bee52818d151be49cf9
Vishnu Sharma <90127810+vishnusharma7@users.noreply.github.com>
2023-07-02 05:54:17 +0530

    Update popupscript.js

--- popupscript.js
+++ popupscript.js
@@ -1,6 +1,6 @@
-// Get the elements
 const workDetail = document.querySelector('.work-detail');
-const popupContainer = document.querySelector('.popup-container');
+const popupContainer = document.querySelector('.workspace-detail');
+const cancelButton = document.querySelector('.button[data-action="cancel"]');
 const dimmedBackground = document.createElement('div');
 dimmedBackground.classList.add('dimmed-background');
 
@@ -10,22 +10,26 @@ workDetail.addEventListener('click', () => {
     document.body.appendChild(dimmedBackground);
 });
 
-// Hide the popup and remove the dimmed background
+// Hide the popup 
 function hidePopup() {
     popupContainer.style.display = 'none';
     document.body.removeChild(dimmedBackground);
 }
 
-// Hide the popup and remove the dimmed background when clicked outside
+//when clicked outside
 window.addEventListener('click', (event) => {
     if (event.target !== workDetail && !popupContainer.contains(event.target)) {
         hidePopup();
     }
 });
 
-// Hide the popup and remove the dimmed background when the escape key is pressed
+// Hide  when the escape key is pressed
 window.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
         hidePopup();
     }
+});
+//hide when click button clicked
+cancelButton.addEventListener('click', () => {
+    hidePopup();
 });
